3c4c5d4d76368c558796e51bfc14f609a737ea63
Wiru <estuardo.quan@gmail.com>
2022-09-05 17:30:10 -0600

    testing routes

--- routes/accounts.php
+++ routes/accounts.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Woops\LaravelPassport\Http\Controllers\AuthenticationController;
 
-Route::view('/login', 'passport-pkce::login');
+Route::view('login', 'passport-pkce::login');
 
 Route::name('login')
     ->middleware('guest')
